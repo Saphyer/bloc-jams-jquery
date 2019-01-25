e4a68cfe9eaebb297373f0a033a00d44b3370831
Saphyer <saphyer@me.com>
2019-01-25 15:58:49 -0500

    assignment-bjj-sliders: completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
@@ -11,7 +11,7 @@
     const nextSongIndex = currentSongIndex +1;
     if (nextSongIndex >= album.songs.length) { return; }
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
 
   $('button#previous').on('click', function() {
@@ -21,12 +21,16 @@
     const previousSongIndex = currentSongIndex -1;
     if (previousSongIndex < -1) { return false; }
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
 
   $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
    });
+
+  $('#volume-control input').on('input', function (event) {
+     player.setVolume(event.target.value);
+    });
 
   setInterval( () => {
      if (player.playState !== 'playing') { return; }
@@ -36,7 +40,4 @@
      $('#time-control .current-time').text( currentTime );
      $('#time-control input').val(percent);
   }, 1000);
-
-
-
 }
